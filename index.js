a913f37802ca0e630b90ae1d22160c2d0d167e4d
Jamesrolandv <vedanajamesroland@gmail.com>
2023-10-22 21:32:23 +0800

    update footer

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-// on scroll=====================================
-// const experiment = document.querySelector('.window');
-// const img = document.querySelector('img');
-// window.addEventListener('scroll', () => {
-//     let wScroll = this.scrollY;
-//     let opacity = (wScroll - experiment.offsetTop + 800) / wScroll;
-//     let offset = Math.min(600, wScroll - experiment.offsetTop + window.innerHeight - 200);
-//     if(wScroll > experiment.offsetTop - window.innerHeight){
-//         img.style.transform = `translateX(-${(offset * .5)}px)`;
-//         document.querySelector('p').style.opacity = opacity;
-//         document.querySelector('p').style.transform = `translateX(${offset * .5}px)`;
-        
-//     }
-// })
 
-
-// let observer = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-        
-//     })
-// }, {
-//     threshold: .2,
-// });
-// observer.observe(experiment);
 
 // preloader========================================================
 const preload = document.getElementById('preloader');
